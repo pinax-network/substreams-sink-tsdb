41e9e40953f83e11e6faa8518eed97f6311767ee
Denis Carriere <carriere.denis@gmail.com>
2023-04-28 13:02:08 -0400

    update appendEpoch

--- src/victoria_metrics.ts
+++ src/victoria_metrics.ts
@@ -1,25 +1,20 @@
 import { Clock } from "substreams";
 import { logger, register } from "substreams-sink-prometheus"
 
-export async function fetchMetrics(epoch: number):Promise<string> {
-    const metrics =  await register.metrics()
-    const arr = [];
-    const lines = metrics.split("\n")
-    for ( const line of lines ) {
-        if (line == "" || line[0] =='#') {
-            arr.push(line)
-            continue
-        }
-        arr.push(`${line} ${epoch}`)
-    }
-    return arr.join('\n')
+export function appendEpoch(metrics: string, epoch: number) {
+    const separator = "\n";
+    return metrics.split(separator).map(line => {
+        if (line == "" || line[0] =='#') return line // comment or empty line
+        return `${line} ${epoch}`
+    }).join(separator);
 }
 
 export async function handleImport(url: string, scrape_interval: number, clock: Clock) {
     if ( !clock.timestamp ) return; // no timestamp (yet
     const epoch = clock.timestamp.toDate().valueOf();
     if ( epoch / 1000 % scrape_interval != 0 ) return; // only handle epoch intervals
-    const body = await fetchMetrics(epoch);
+    const metrics = await register.metrics();
+    const body = appendEpoch(metrics, epoch);
     await fetch(url, { method: 'POST', body }).catch((error) => {
         logger.error(error)
     });
