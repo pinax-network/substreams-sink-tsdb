c5fe138fb96aede399e790ab3fdf358248d9a50e
Denis Carriere <carriere.denis@gmail.com>
2023-04-28 12:54:52 -0400

    add default labels

--- bin/cli.ts
+++ bin/cli.ts
@@ -3,11 +3,14 @@
 import { cli } from "substreams-sink";
 import { action, DEFAULT_ADDRESS, DEFAULT_PORT, DEFAULT_SCRAPE_INTERVAL } from "../index"
 import pkg from "../package.json";
+import { DEFAULT_COLLECT_DEFAULT_METRICS, handleLabels } from "substreams-sink-prometheus";
 
 const program = cli.program(pkg);
 const command = cli.run(program, pkg);
-command.option('-i --scrape_interval <int>', 'Scrape Interval', String(DEFAULT_SCRAPE_INTERVAL));
 command.option('-p --port <int>', 'Listens on port number.', String(DEFAULT_PORT))
 command.option('-a --address <string>', 'VictoriaMetrics address to connect.', DEFAULT_ADDRESS);
+command.option('-i --scrape_interval <int>', 'Scrape Interval', String(DEFAULT_SCRAPE_INTERVAL));
+command.option('-l --labels [...string]', "To apply generic labels to all default metrics (ex: --labels foo=bar)", handleLabels, {})
+command.option('--collect-default-metrics <boolean>', "Collect default metrics", DEFAULT_COLLECT_DEFAULT_METRICS);
 command.action(action);
 program.parse();
--- index.ts
+++ index.ts
@@ -3,9 +3,7 @@ import { run, logger, RunOptions } from "substreams-sink";
 import pkg from "./package.json";
 
 import { handleImport } from "./src/victoria_metrics";
-import { handleClock, handleManifest, handleOperations, register } from "substreams-sink-prometheus";
-import client from 'prom-client';
-client.collectDefaultMetrics({ register, labels: { foo: "bar" } });
+import { collectDefaultMetrics, handleClock, handleManifest, handleOperations, setDefaultLabels } from "substreams-sink-prometheus";
 
 logger.defaultMeta = { service: pkg.name };
 export { logger };
@@ -15,18 +13,25 @@ export const DEFAULT_ADDRESS = '0.0.0.0';
 export const DEFAULT_PORT = 8428;
 export const DEFAULT_SCRAPE_INTERVAL = 30;
 export const TYPE_NAME = 'pinax.substreams.sink.prometheus.v1.PrometheusOperations';
+export const DEFAULT_COLLECT_DEFAULT_METRICS = false;
 
 // Custom user options interface
 export interface ActionOptions extends RunOptions {
     address: string;
     port: number;
     scrape_interval: number;
+    labels: Object;
+    collectDefaultMetrics: boolean;
 }
 
 export async function action(manifest: string, moduleName: string, options: ActionOptions) {
     // Get command options
     const { address, port, scrape_interval} = options;
     const url = `http://${address}:${port}/api/v1/import/prometheus`
+
+    // Set default labels
+    if ( options.collectDefaultMetrics ) collectDefaultMetrics(options.labels);
+    if ( options.labels ) setDefaultLabels(options.labels);
 
     // Download substreams
     const spkg = await download(manifest);
